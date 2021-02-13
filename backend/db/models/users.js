464d8dd1c9a1ffb57f44dfb9440c7d784ccf3b24
Egor Shumeyko <tierdal@hotmail.com>
2021-02-13 14:04:55 -0600

    update the register page and use the main users table for auth

---
+++ backend/db/models/users.js
@@ -0,0 +1,52 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('users', {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    f_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    l_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    department_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    is_approver: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'users',
+    schema: 'dbo',
+    timestamps: true,
+    underscored: true,
+    createdAt: 'CREATED_AT',
+    updatedAt: 'UPDATED_AT',
+    deletedAt: false
+  });
+};
