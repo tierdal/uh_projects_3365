8a87e84fecc6204b3ac219cde0a74fa8ed4d5a0c
Egor Shumeyko <tierdal@hotmail.com>
2021-02-25 18:20:27 -0600

    comment out some obsolete code

--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="jumbotron">
-    <h1 class="display-3">Demo</h1>
-    <p>Demo System.</p>
+    home page content goes here
+    <!--<h1 class="display-3">Demo</h1>
+    <p>Demo System.</p>-->
   </div>
 </template>
 
